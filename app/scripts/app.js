133565d2abdf98804b31094b56b2ccdfcaa36ffb
Brookelyn <brooke.balza@gmail.com>
2015-11-09 09:13:11 +0000

    Users now able to take a break after task

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,10 @@ var bloctime = angular.module("bloctime",  ["ui.router", "firebase"]);
 
 // Set-up
 
+// bloctime.constant('TIME_TASK', 1500);
+// bloctime.constant('TIME_BREAK', 300);
+// bloctime.constant('TIME_LONG_BREAK', 1800);
+
 // Homepage controller
 
 bloctime.controller('mainCtlr', function($scope, $interval) {
@@ -25,32 +29,75 @@ bloctime.directive('clock', ['$interval',  function($interval) {
     scope: true,
     link: function(scope, element, attributes) {
       
-        scope.tasktime = 1500;
-        scope.isTimerRunning = false;
+        scope.tasktime = 5;
         scope.buttonText = "Start";
+        scope.breakButtonText = "Start break";
+        scope.isTimerRunning = false;
+        scope.isBreakRunning = false;
+        scope.onBreak = false;
 
 
     // Sets functionality for start/reset timer button
     scope.toggleTimer = function() {
       if (scope.isTimerRunning === false) {
-        
         scope.timerInterval = $interval(function(){
           scope.tasktime--;
         }, 1000);
-
         scope.isTimerRunning = true;
         scope.buttonText = "Reset";
       }
       else {
         scope.isTimerRunning = false;
-        scope.tasktime = 1500;
+        scope.tasktime = 5;
         $interval.cancel(scope.timerInterval);
         scope.buttonText = "Start";
       }
     };
 
-    // Changes button classes based on buttonText
 
+
+    // Sets functionality for break button
+    scope.toggleBreak = function() {
+      if (scope.isBreakRunning === false) {
+        scope.timerInterval = $interval(function(){
+          scope.tasktime--;
+        }, 1000);
+        scope.isBreakRunning = true;
+        scope.breakButtonText = "Pause";
+      }
+      else {
+        $interval.cancel(scope.timerInterval);
+        scope.isBreakRunning = false;
+        scope.breakButtonText = "Restart break";
+      }
+    };
+
+
+    // Stops all timer countdowns at 00:00 and sets correct time for either task or break
+    scope.expireTimer = function(){
+      if (scope.tasktime === 0){
+        if (scope.isTimerRunning === true) {
+          $interval.cancel(scope.timerInterval);
+          scope.isTimerRunning = false;
+          scope.onBreak = true;
+          scope.tasktime = 3;
+          scope.breakButtonText = "Start break";
+          scope.buttonText = "Start";
+        }
+        if (scope.isBreakRunning === true) {
+          $interval.cancel(scope.timerInterval);
+          scope.onBreak = false;
+          scope.isBreakRunning = false;
+          scope.tasktime = 5;
+          scope.buttonText = "Start";
+        }
+      }
+    };
+
+
+    
+
+    // Changes button classes based on buttonText
     scope.setButtonClass = function() {
       if (scope.buttonText === "Start"){
         return 'animated infinite pulse';
@@ -62,7 +109,16 @@ bloctime.directive('clock', ['$interval',  function($interval) {
 
 
 
-    
+    // Changes break button classes based on breadButtonText
+    scope.setBreakButtonClass = function() {
+      if (scope.breakButtonText === "Start break"){
+        return 'animated infinite pulse';
+      }
+      if (scope.buttonText === "Pause break"){
+        return '';
+      }
+    };
+
 
 
     }
